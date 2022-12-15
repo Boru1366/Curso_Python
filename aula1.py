a3320fcf0ebae5427f804eac144ea88e22228921
Boru1366 <120612302+Boru1366@users.noreply.github.com>
2022-12-15 17:41:31 -0300

    Add files via upload

---
+++ aula1.py
@@ -0,0 +1,14 @@
+"""
+DocString => um formato de comentário utilizando várias linhas utilizando 3 aspas dublas.  
+
+
+"""
+'''
+Sendo utilizado esta forma de 3 aspas simples para fazer comentários com várias linhas.
+
+'''
+
+# Permite escreve um comentário.
+print('123')  # aqui poderia ter outro comentario sobre o comando.
+# Abaixo
+print('123')
